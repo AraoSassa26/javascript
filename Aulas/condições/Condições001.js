87e5431658416474ba4b6bbe6732d5a8d91df3aa
Arão Sassa <156106525+sassaDev2024@users.noreply.github.com>
2025-03-13 09:54:57 +0100

    03/2025

---
+++ Aulas/condições/Condições001.js
@@ -0,0 +1,9 @@
+var vel = 60.55
+
+console.log(`A velocidade do seu carro é ${vel}km/h `)
+
+if (vel>60){ // condição simples
+    console.log(`Você ultrapassou a velocidade limite , MULTADO!!`)
+}
+
+console.log(`Dirija sempre com cinto de segurança`)
